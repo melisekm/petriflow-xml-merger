cde86d061d4bff2f19eaac596022a9103015e1c0
melisekm <46221266+melisekm@users.noreply.github.com>
2022-04-17 19:41:56 +0200

    heroku integration

--- src/app.py
+++ src/app.py
@@ -46,6 +46,5 @@ def home():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.secret_key = 'the random string'
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
